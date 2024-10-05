40b55f266ea8ea64e6ee9d4ff60f73c0847d0cde
Chloe <zandbergchloe@gmail.com>
2024-10-05 13:00:01 -0700

    renamed app.py to bruh.py for git

---
+++ bruh.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import time
+
+"""
+## Your eco-friendly image generator: [INSERT NAME HERE]
+#### A memoized stable diffusion image generator
+"""
