5ef3e90973311c209a07211127a2d0f9a988370a
Andrew Ahn <andrewahn21@gmail.com>
2024-10-05 18:11:44 -0700

    made everything work LESGOOO

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import boto3
+import io
 import json
 from botocore.exceptions import ClientError
 import base64
+from PIL import Image
 # Initialize the Bedrock client
 client = boto3.client(service_name='bedrock-runtime')
 
@@ -11,6 +13,10 @@ boto3.client('dynamodb')
 dynamoClient = boto3.client(service_name="dynamodb", region_name="us-west-2")
 db = boto3.resource('dynamodb')
 promptTable = db.Table(__TableName__)
+
+# Initialize the S3 client
+s3_client = boto3.client('s3')
+bucket_name = 'cichackathon2024'
 
 def generate_image(prompt):
     # Define the request payload
@@ -22,8 +28,8 @@ def generate_image(prompt):
         },
         "imageGenerationConfig": {
             "numberOfImages": 1,
-            "height": 1024,
-            "width": 1024,
+            "height": 512,
+            "width": 512,
             "cfgScale": 8.0,
             "seed": 0
         }
@@ -47,10 +53,118 @@ def generate_image(prompt):
     base64_image = result.get("images")[0]
     base64_bytes = base64_image.encode('ascii')
     image_bytes = base64.b64decode(base64_bytes)
-
-    finish_reason = result.get("error")
     
-    return base64_image
+    return image_bytes
+
+def upload_image_to_s3(image_bytes, prompt):
+    s3_key = f'images/{prompt}.png'  # Unique S3 key for each prompt
+    s3_client.put_object(Bucket=bucket_name, Key=s3_key, Body=image_bytes, ContentType='image/png')
+    # return f'https://{bucket_name}.s3.amazonaws.com/{s3_key}'
+    return s3_key
+
+def generate_image_and_store(prompt):
+    image_bytes = generate_image(prompt)  # Assuming generate_image returns binary data
+    
+    # Upload image to S3
+    image_key = upload_image_to_s3(image_bytes, prompt)
+
+    # Store prompt and S3 URL in DynamoDB
+    promptTable.put_item(
+        Item={
+            'prompt': prompt,
+            'image_url': image_key  # Storing S3 URL instead of Base64 data
+        }
+    )
+    
+    return image_key  # Return the URL for display in the app
+
+def getOrGenerate(prompt):    
+    # Contact llm with this prompt
+    existingPrompt = findExistingPrompt(prompt)
+    existingPrompt = existingPrompt.strip()
+    if ("None" != existingPrompt):
+        entry = promptTable.get_item(
+            Key={
+                'prompt': existingPrompt
+            }
+        )
+        print(entry["Item"])
+        image_key = entry["Item"]["image_url"]
+        return image_key
+    else:
+        new_image_key = generate_image_and_store(prompt)
+        # promptTable.put_item(
+        #     Item={
+        #         'prompt': prompt,
+        #         'image': new_image
+        #     }
+        # )
+        
+        return new_image_key
+    
+def extractPrompts(item):
+    return item["prompt"]
+def findExistingPrompt(prompt):
+    listPrompts = promptTable.scan()['Items']
+    prompts = map(extractPrompts, listPrompts)
+    cached_prompts_array = list(prompts) #get keys from db
+
+    cached_prompts = ""
+
+    # turn cached prompt array file into one string separated by '|'
+    for existingPrompt in cached_prompts_array:
+        cached_prompts = cached_prompts + " | " + existingPrompt
+    # Adding one final "| and adding the rest of the prompt for LLM"
+    cached_prompts = cached_prompts + " | Is there anything in this list of prompts (separated by the character '|') that is sufficiently and semantically the same to the prompt '" + prompt + "'? If there is, return it exactly without saying anything else. If not, return 'None'."
+
+    return contactLLM(cached_prompts)  
+    
+def contactLLM(prompt):
+    # Create a Bedrock Runtime client in the AWS Region of your choice.
+    client = boto3.client("bedrock-runtime", region_name="us-west-2")
+
+    # Set the model ID, e.g., Llama 3 70b Instruct.
+    model_id = "meta.llama3-70b-instruct-v1:0"
+
+    # Embed the prompt in Llama 3's instruction format.
+    formatted_prompt = f"""
+    <|begin_of_text|><|start_header_id|>user<|end_header_id|>
+    {prompt}
+    <|eot_id|>
+    <|start_header_id|>assistant<|end_header_id|>
+    """
+
+    # Format the request payload using the model's native structure.
+    native_request = {
+        "prompt": formatted_prompt,
+        "max_gen_len": 512,
+        "temperature": 0.5,
+    }
+
+    # Convert the native request to JSON.
+    request = json.dumps(native_request)
+
+    out = ""
+
+    try:
+        # Invoke the model with the request.
+        streaming_response = client.invoke_model_with_response_stream(
+            modelId=model_id, body=request
+        )
+
+        # Extract and print the response text in real-time.
+        for event in streaming_response["body"]:
+            chunk = json.loads(event["chunk"]["bytes"])
+            if "generation" in chunk:
+                # print(chunk["generation"], end="")
+                out = out + chunk["generation"]
+
+    except (ClientError, Exception) as e:
+        print(f"ERROR: Can't invoke '{model_id}'. Reason: {e}")
+        exit(1)
+
+    return out
+
 
 # Streamlit app
 st.title("Amazon Titan Image Generator")
@@ -58,115 +172,12 @@ prompt = st.text_input("Enter your text prompt:")
 
 if st.button("Generate Image"):
     with st.spinner("Generating image..."):
-        image_data = generate_image(prompt)
-        image = base64.b64decode(image_data)
-        st.image(image, caption="Generated Image")
-
-# Use the native inference API to send a text message to Meta Llama 3.
-import boto3
-import json
-
-from botocore.exceptions import ClientError
-
-# Create a Bedrock Runtime client in the AWS Region of your choice.
-client = boto3.client("bedrock-runtime", region_name="us-west-2")
-
-# Set the model ID, e.g., Llama 3 70b Instruct.
-model_id = "meta.llama3-70b-instruct-v1:0"
-
-# Define the prompt for the model.
-
-# Embed the prompt in Llama 3's instruction format.
-formatted_prompt = f"""
-<|begin_of_text|><|start_header_id|>user<|end_header_id|>
-{prompt}
-<|eot_id|>
-<|start_header_id|>assistant<|end_header_id|>
-"""
-
-# Format the request payload using the model's native structure.
-native_request = {
-    "prompt": formatted_prompt,
-    "max_gen_len": 512,
-    "temperature": 0.5,
-}
-
-# Convert the native request to JSON.
-request = json.dumps(native_request)
-
-try:
-    # Invoke the model with the request.
-    streaming_response = client.invoke_model_with_response_stream(
-        modelId=model_id, body=request
-    )
-
-    # Extract and print the response text in real-time.
-    for event in streaming_response["body"]:
-        chunk = json.loads(event["chunk"]["bytes"])
-        if "generation" in chunk:
-            print(chunk["generation"], end="")
-
-except (ClientError, Exception) as e:
-    print(f"ERROR: Can't invoke '{model_id}'. Reason: {e}")
-    exit(1)
-
-
-if __name__ == "__main__":
-    main()
-
-__TableName__ = "promptCache"
-boto3.client('dynamodb')
-dynamoClient = boto3.client(service_name="dynamodb", region_name="us-west-2")
-db = boto3.resource('dynamodb')
-promptTable = db.Table(__TableName__)
-
-def getOrGenerate(prompt):    
-    # Contact llm with this prompt
-    existingPrompt = findExistingPrompt(prompt)
-    if (existingPrompt != "None"):
-        entry = promptTable.get_item(
-            Key={
-                'prompt': existingPrompt
-            }
-        )
-        base64ImageData = entry["Item"]
-        return base64ImageData
-    else:
-        new_image = generate_image(prompt)
-        promptTable.put_item({
-            {
-                'prompt': prompt,
-                'image': new_image
-            }
-        })
-        return new_image
-    
-
-def findExistingPrompt(prompt):
-    cached_prompts_array = [] #get keys from db
-
-    # turn cached prompt array file into one string separated by '|'
-    for existingPrompt in (cached_prompts_array):
-        cached_prompts = cached_prompts + " | " + existingPrompt
-    # Adding one final "| and adding the rest of the prompt for LLM"
-    cached_prompts = cached_prompts + " | Is there anything in this list of prompts (separated by the character '|') that is sufficiently and semantically the same to the prompt '" + prompt + "'? If there is, return it exactly without saying anything else. If not, return 'None'."
-
-    try:
-        return (contactLLM(cached_prompts))
-    except:
-        return "None"
-    
-def contactLLM(prompt):
-    #TODO send prompt to LLM and get respose back
-    #Parse response to get necessary shit
-    return #TODO prompt
-
-if st.button("Generate Image"):
-    with st.spinner("Generating image..."):
-        checkOrGenerate(prompt)
-        image_data = generate_image(prompt)
-        image = base64.b64decode(image_data)
-        st.image(image, caption="Generated Image")
+        image_key = getOrGenerate(prompt)
+        print(image_key)
+        image_response = s3_client.get_object(Bucket=bucket_name, Key=image_key)
+        image_base64 = image_response['Body'].read()
+        # image = base64.b64decode(image_base64)
+        st.image(image_base64, caption="Generated Image")
 
 
 # def checkOrGenerate(prompt):    
